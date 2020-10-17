93042d04b645c624465f1fa917d4ce436a520ad3
adrien goux <adriengoux@gmail.com>
2020-10-17 17:15:57 +0800

    app done

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+100.times {
+  article = Article.new(
+    title:    Faker::Company.name,
+    content:  Faker::Quotes::Shakespeare.hamlet_quote + Faker::Quotes::Shakespeare.as_you_like_it_quote,
+    url:      Faker::Internet.url,
+    author:   Faker::Name.name,
+    rating: rand(0..5)
+    )
+  article.save!
+}
